0705e485f6ee0012ea3c10c83306cf4ebd6cf324
Darshan Jain <djainj86@gmail.com>
2023-03-17 16:27:10 -0400

    added custom textinput component

---
+++ AssignNova/controller/TextInput.swift
@@ -0,0 +1,104 @@
+//
+//  TextInput.swift
+//  AssignNova
+//
+//  Created by Darshan Jain on 2023-03-17.
+//
+
+import UIKit
+
+@IBDesignable
+class TextInput: UIView {
+	
+	static let identifier = "TextInput"
+
+	@IBOutlet var contentView: UIView!
+	@IBOutlet weak var labelComponent: UILabel!
+	@IBOutlet weak var textFieldComponent: UITextField!
+	
+	@IBInspectable
+	var label: String?{
+		didSet{
+			self.labelComponent.text = label
+		}
+	}
+	
+	@IBInspectable
+	var placeholder: String?{
+		didSet{
+			self.textFieldComponent.placeholder = placeholder
+		}
+	}
+	
+	@IBInspectable
+	var leftIcon: UIImage?{
+		didSet{
+			if leftIcon != nil {
+				textFieldComponent.leftViewMode = .always
+				let imageView = UIImageView(frame: CGRect(x: 6, y: 0, width: 20, height: 20))
+				imageView.contentMode = .scaleAspectFit
+				imageView.image = leftIcon
+				let view = UIView(frame: CGRect(x: 0, y: 0, width: 32, height: 20))
+				view.addSubview(imageView)
+				textFieldComponent.leftView = view
+			} else {
+				textFieldComponent.leftViewMode = .never
+				textFieldComponent.leftView = nil
+			}
+		}
+	}
+	
+	@IBInspectable
+	var leftIconColor: UIColor?{
+		didSet{
+			textFieldComponent?.leftView?.tintColor = leftIconColor
+		}
+	}
+	
+	@IBInspectable
+	var rightIcon: UIImage?{
+		didSet{
+			if leftIcon != nil {
+				textFieldComponent.rightViewMode = .always
+				let imageView = UIImageView(frame: CGRect(x: 6, y: 0, width: 20, height: 20))
+				imageView.contentMode = .scaleAspectFit
+				imageView.image = rightIcon
+				let view = UIView(frame: CGRect(x: 0, y: 0, width: 32, height: 20))
+				view.addSubview(imageView)
+				textFieldComponent.rightView = view
+			} else {
+				textFieldComponent.rightViewMode = .never
+				textFieldComponent.rightView = nil
+			}
+		}
+	}
+	
+	@IBInspectable
+	var rightIconColor: UIColor?{
+		didSet{
+			textFieldComponent?.rightView?.tintColor = rightIconColor
+		}
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		initSubviews()
+	}
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		initSubviews()
+	}
+	
+	func initSubviews() {
+		
+		let bundle = Bundle(for: TextInput.self)
+		bundle.loadNibNamed(TextInput.identifier, owner: self, options: nil)
+		contentView.frame = bounds
+		contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+		
+		addSubview(contentView)
+		
+	}
+	
+}
