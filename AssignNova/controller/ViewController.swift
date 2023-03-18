8a3f6301921ef5508481f1dbfd428d63bc196432
Darshan Jain <djainj86@gmail.com>
2023-03-18 11:23:39 -0400

    completed verify mobile number mobile ui

--- AssignNova/controller/ViewController.swift
+++ AssignNova/controller/ViewController.swift
@@ -8,18 +8,10 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
+import AEOTPTextField
 
-class ViewController: UIViewController, SelectLocationDelegate {
-	func onSelectLocation(place: GMSPlace) {
-		print(place)
-	}
+class ViewController: UIViewController {
 	
-	func onCancel() {
-		print("cancelled")
-	}
-	
-
-	@IBOutlet weak var testButton: UIButton!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
@@ -27,6 +19,8 @@ class ViewController: UIViewController, SelectLocationDelegate {
 //		let camera = GMSCameraPosition(latitude: 1.285, longitude: 103.848, zoom: 12)
 //		let mapView = GMSMapView(frame: .zero, camera: camera)
 //		self.view = mapView
+		
+		
 	}
 
 	@IBAction func onTestButtonPress(_ sender: Any) {
@@ -34,5 +28,21 @@ class ViewController: UIViewController, SelectLocationDelegate {
 					 animated:true, completion: nil)
 	}
 	
+	@IBAction func onOpenOtpInputPress(_ sender: Any) {
+		let otpInputController = UIStoryboard(name: "OtpInput", bundle: nil)
+			.instantiateViewController(withIdentifier: "OtpInputViewController") as! OtpInputViewController
+		
+		navigationController?.pushViewController(otpInputController, animated: true)
+	}
+	
 }
 
+extension ViewController: SelectLocationDelegate {
+	func onSelectLocation(place: GMSPlace) {
+		print(place)
+	}
+	
+	func onCancel() {
+		print("cancelled")
+	}
+}
