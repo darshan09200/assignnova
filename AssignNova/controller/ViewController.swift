5f35e8e6051bfbfdcf84226e80d4308aa3227ea3
Darshan Jain <djainj86@gmail.com>
2023-03-21 19:29:19 -0400

    removed test ui from start screen

--- AssignNova/controller/ViewController.swift
+++ AssignNova/controller/ViewController.swift
@@ -17,44 +17,15 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 
-//		let camera = GMSCameraPosition(latitude: 1.285, longitude: 103.848, zoom: 12)
-//		let mapView = GMSMapView(frame: .zero, camera: camera)
-//		self.view = mapView
-
 	}
 
-	@IBAction func onTestButtonPress(_ sender: Any) {
-//		self.present(SelectLocationViewController.getController(selectLocationDelegate: self),
-//					 animated:true, completion: nil)
-        //UIStoryboard(name: "SignInScreen", bundle: nil).instantiateViewController(withIdentifier: "SignIn")
+	@IBAction func onSignInPress(_ sender: Any) {
         navigationController?.pushViewController(UIStoryboard(name: "SignInScreen", bundle: nil).instantiateViewController(withIdentifier: "SignInVC"), animated: true)
-	}
-
-	@IBAction func onOpenOtpInputPress(_ sender: Any) {
-		let viewController = UIStoryboard(name: "Branch", bundle: nil)
-			.instantiateViewController(withIdentifier: "AddBranchVC")
-		self.present(UINavigationController(rootViewController: viewController), animated: true)
 	}
 
 	@IBAction func onOpenSignUpBusinessPress(_ sender: Any) {
 		let signUpBusinessVC = UIStoryboard(name: "SignUpBusiness", bundle: nil)
 			.instantiateViewController(withIdentifier: "SignUpBusinessAccountVC") as! SignUpBusinessAccountVC
 		self.navigationController?.pushViewController(signUpBusinessVC, animated: true)
-	}
-
-	@IBAction func onOpenSetupBusinessPress(_ sender: Any) {
-		let setupBusinessVC = UIStoryboard(name: "SignUpBusiness", bundle: nil)
-			.instantiateViewController(withIdentifier: "SetupBusinessVC") as! SetupBusinessVC
-		self.navigationController?.pushViewController(setupBusinessVC, animated: true)
-	}
-}
-
-extension ViewController: SelectLocationDelegate {
-	func onSelectLocation(place: GMSPlace) {
-		print(place)
-	}
-
-	func onCancelLocation() {
-		print("cancelled")
 	}
 }
