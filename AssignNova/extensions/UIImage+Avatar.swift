2d96148150b65bf71cc661a7995e3c74ab8b2b57
Darshan Jain <djainj86@gmail.com>
2023-03-26 08:27:25 -0400

    added view all employees, view employee, edit employees mobile functionality

---
+++ AssignNova/extensions/UIImage+Avatar.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage+Avatar.swift
+//  AssignNova
+//
+//  Created by Darshan Jain on 2023-03-25.
+//
+
+import Foundation
---
+++ AssignNova/view/EmployeeDetailsCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmployeeDetailsCell.swift
+//  AssignNova
+//
+//  Created by Darshan Jain on 2023-03-26.
+//
+
+import UIKit
+
+class EmployeeDetailsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
